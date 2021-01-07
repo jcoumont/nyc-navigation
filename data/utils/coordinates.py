5b875e9189bd3474effb7a91f0e2ac89493d2204
GodIsADJ <axelle.paquet+cv@gmail.com>
2021-01-07 14:20:56 +0100

    Get coordinates from address

---
+++ data/utils/coordinates.py
@@ -0,0 +1,43 @@
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+
+
+class LocationUnknownException(Exception):
+    pass
+
+
+class LocationNotInNyException(Exception):
+    pass
+
+
+def __do_geocode(address: str, attempt: int = 1, max_attempts: int = 5) -> object:
+    geolocator = Nominatim(user_agent="nyc-navigation")
+    try:
+        return geolocator.geocode(address)
+    except GeocoderTimedOut:
+        if attempt <= max_attempts:
+            return __do_geocode(address, attempt=attempt + 1)
+        raise
+
+
+def get_coordinates(address: str):
+    location = __do_geocode(address)
+    if location == None:
+        # return "unknown address"
+        raise LocationUnknownException
+    elif "New York" not in location.address:
+        raise LocationNotInNyException
+    else:
+        return (location.latitude, location.longitude)  # , location.address)
+
+
+if __name__ == "__main__":
+    try:
+        print(get_coordinates("175 5th Avenue NYC"))
+        print(get_coordinates("uguugvuv"))
+        print(get_coordinates("Liège"))
+    except LocationUnknownException:
+        print("Unknown address")
+    except LocationNotInNyException:
+        print("Address not in New York")
+
