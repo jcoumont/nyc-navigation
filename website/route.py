7d06c85127404638cba923fd88e46d0143ecd935
root <axelle.paquet+git@gmail.com>
2021-01-11 18:50:44 +0000

    Ok

--- website/route.py
+++ website/route.py
@@ -1,45 +1,57 @@
 from flask import Flask, request, render_template
-from utils.coordinates import get_coordinates, LocationUnknownException, LocationNotInNyException
-from utils.map import get_map
+from utils.coordinates import (
+    get_coordinates,
+    LocationUnknownException,
+    LocationNotInNyException,
+)
+from utils.NYCMapManager import NYCMapManager
 from utils.NYCRouteManager import NYCRouteManager
 import os
 
 
 app = Flask(__name__, template_folder=".")
 route_manager = NYCRouteManager()
+map_manager = NYCMapManager()
+
 
 @app.route("/")
 def hello():
-    """Display the home page
-    """
+    """Display the home page"""
+    # data: str = "Hello World"
+    # return render_template("default.html", title="Home", data=data)
     return render_template("default.html", title="Home")
-
 
 
 @app.route("/navigate", methods=["POST"])
 def login():
-    """Get the coordinates from adrress
-    """
+    """Get the coordinates from adrress"""
 
-    from_address = request.form['from_address']
-    to_address = request.form['to_address']
+    from_address = request.form["from_address"]
+    to_address = request.form["to_address"]
+    route_types = ["safest"]
+    route_types.extend(request.form.getlist('type'))
 
     try:
         start_location = get_coordinates(from_address)
         end_location = get_coordinates(to_address)
 
-        route = route_manager.get_safest_route(start_location, end_location)
-        data = get_map(start_location, end_location, route=route)
-        return render_template("default.html", title="Navigation",data=data)
+        routes = route_manager.get_routes(start_location, end_location, route_types)
+        data = map_manager.get_map(start_location, end_location, routes=routes)
+
+        return render_template("default.html", title="Navigation", data=data)
         # return render_template("default.html", title="Navigation", start_location=start_location, end_location=end_location)
     except LocationUnknownException:
-        return render_template("default.html", title="Navigation : Error", data="Unknown address")
+        return render_template(
+            "default.html", title="Navigation : Error", data="Unknown address"
+        )
     except LocationNotInNyException:
-        return render_template("default.html", title="Navigation : Error", data="Address not in New York")
+        return render_template(
+            "default.html", title="Navigation : Error", data="Address not in New York"
+        )
     except Exception:
-        return render_template("default.html", title="Navigation : Error", data="Error!")
-
-
+        return render_template(
+            "default.html", title="Navigation : Error", data="Error!"
+        )
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
