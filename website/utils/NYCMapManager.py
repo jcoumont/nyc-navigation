561ce4a3d1eea5ab352e641dc9f9a53aaafa378c
jcoumont <jcoumont@gmail.com>
2021-01-12 18:10:52 +0100

    BUGFIX Map didn't refresh after recompute Remove the global map and copy which is the source of the problem The Map is recreated at each time Some code is commented It allows to displays the clustering of the crashes on the map. This method give a really bad response time Then I commented the code but keep it here for a future Maybe try to save this card has an HTML to display it without recomposing it

--- website/utils/NYCMapManager.py
+++ website/utils/NYCMapManager.py
@@ -1,11 +1,15 @@
-import copy
 import folium
 import pandas as pd
 import geopandas as gpd
+from shapely.geometry import Point
+from folium import plugins
 
 
 class NYCMapManager:
-    default_map = None
+    latitude = 40.677834
+    longitude = -74.012443
+    crashes_filepath = 'data/NYC_crashes_100000_osmid.csv'
+    layer_crashes = None
     default_colors = {
                         "shortest": "blue",
                         "safest": "green",
@@ -13,15 +17,48 @@ class NYCMapManager:
                         "other": "yellow"
                      }
 
-    def __init__(self):
-        latitude = 40.677834
-        longitude = -74.012443
-        self.default_map = folium.Map(location=[latitude, longitude], zoom_start=10)
-        #self.initialize_accident_layer()
+    # def __init__(self):
+    #     self.initialize_crashes_layer()
 
-    def initialize_accident_layer(self):
-        layer_group = folium.FeatureGroup(name="accident").add_to(self.default_map)
+    # def initialize_crashes_layer(self):
+    #     df_risk = pd.read_csv(self.crashes_filepath)
+    #     geom = [Point(xy) for xy in zip(df_risk.longitude, df_risk.latitude)]
+    #     gdf_risk = gpd.GeoDataFrame(df_risk, crs="EPSG:4326", geometry=geom)
 
+    #     self.layer_crashes = folium.FeatureGroup(name="crashes")
+
+    #     marker_cluster = plugins.MarkerCluster()
+    #     self.layer_crashes.add_child(marker_cluster)
+    #     for i, v in gdf_risk.iterrows():
+    #         popup = """
+    #         Killed : <b>%s</b><br>
+    #         Injured : <b>%s</b><br>
+    #         """ % (v['persons_killed'], v['persons_injured'])
+
+    #         if v['persons_killed'] > 0:
+    #             folium.CircleMarker(location=[v['latitude'], v['longitude']],
+    #                                 radius=10,
+    #                                 tooltip=popup,
+    #                                 color='#581845',
+    #                                 fill_color='#581845',
+    #                                 fill_opacity=0.7,
+    #                                 fill=True).add_to(marker_cluster)
+    #         elif v['persons_injured'] > 0:
+    #             folium.CircleMarker(location=[v['latitude'], v['longitude']],
+    #                                 radius=10,
+    #                                 tooltip=popup,
+    #                                 color='#C70039',
+    #                                 fill_color='#C70039',
+    #                                 fill_opacity=0.7,
+    #                                 fill=True).add_to(marker_cluster)
+    #         else:
+    #             folium.CircleMarker(location=[v['latitude'], v['longitude']],
+    #                                 radius=10,
+    #                                 tooltip=popup,
+    #                                 color='#FFC300',
+    #                                 fill_color='#FFC300',
+    #                                 fill_opacity=0.7,
+    #                                 fill=True).add_to(marker_cluster)
 
     def get_map(self, from_coord: tuple, to_coord: tuple, routes=None):
         """Return the html code to display map
@@ -33,7 +70,8 @@ class NYCMapManager:
         Returns:
             map_nyc._repr_html_(): the map in html format
         """
-        map_nyc = copy.copy(self.default_map)
+        map_nyc = folium.Map(location=[self.latitude, self.longitude], zoom_start=10)
+        #self.layer_crashes.add_to(map_nyc)
 
         if routes is not None:
             if type(routes) is dict:
